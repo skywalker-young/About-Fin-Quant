d13f0809067f6f67b0c6975f836575bcafcd8713
膜法师 <30488918+skywalker-young@users.noreply.github.com>
2017-08-02 17:33:54 +1000

    Create ch1.py

---
+++ ch1.py
@@ -0,0 +1,10 @@
+# python-for-finance
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+x=np.linspace(0,2*math.pi)
+plt.plot(x,np.sin(x),label=r'xx')
+plt.plot(x,np.cos(x),'ro')
+plt.title(r'two plots in a graph')
+plt.legend
